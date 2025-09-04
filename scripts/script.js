b65916ec9d9d8a5cafef92ed8fa4a5a449fdee11
TZGaming <tomzuidervaart@gmail.com>
2025-09-04 11:51:53 +0200

    banana gif

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,19 @@ function play() {
   var audio = document.getElementById("test");
     audio.play();
 }
+
+  const image = document.getElementById('Bananza_icn');
+
+  image.addEventListener('click', () => {
+    const originalSrc = image.getAttribute('data-original');
+    const gifSrc = image.getAttribute('data-gif');
+    const duration = parseInt(image.getAttribute('data-duration'), 10); // in ms
+
+    // Vervang door de GIF
+    image.src = gifSrc;
+
+    // Zet terug na de duur van de GIF
+    setTimeout(() => {
+      image.src = originalSrc;
+    }, duration);
+  });
